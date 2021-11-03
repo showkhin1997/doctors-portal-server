cfb54abd02e3ffc3293d0252ddf259b467879dae
showkhin1997 <showkhin15@gmail.com>
2021-11-03 18:00:41 +0600

    doctor-server

---
+++ index.js
@@ -0,0 +1,39 @@
+
+
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+// middleweare
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6re1r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+
+        console.log('database connected')
+
+    }
+
+    finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello Doctors Portal')
+});
+
+app.listen(port, () => {
+    console.log(`listening at ${port}`)
+});
