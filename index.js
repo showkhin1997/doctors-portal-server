4f6fc55fd436efa4d704c9facac616ec7fd4b22a
showkhin1997 <showkhin15@gmail.com>
2021-11-17 22:02:55 +0600

    middleweare

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6re1r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+console.log(uri)
+
 async function verifyToken(req, res, next) {
 
     if (req.headers?.authorization?.startWith('Bearer ')) {
